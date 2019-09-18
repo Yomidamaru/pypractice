7b10e85b5ccceb614ef88a6fd3c0d5a093332e96
Yo Kamiya <you@example.com>
2019-09-17 17:43:31 -0700

    first commit

---
+++ colors.py
@@ -0,0 +1,17 @@
+from termcolor import colored
+from pyfiglet import figlet_format 
+
+def print_art(msg, color):
+	valid_colors = ("red", "green", "yellow", "blue", "magenta", "cyan", "white")
+
+	if color not in valid_colors:
+		color = "magenta"
+
+	ascii_art = figlet_format(msg)
+	colored_ascii = colored(ascii_art, color=color)
+	print(colored_ascii)
+
+msg = input("What would you like to print? ")
+color = input("What color? ")
+print_art(msg, color)
+
